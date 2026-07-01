f5ebbcae5610714a2f176352c015dd83e985435f
Aditya <akash.aditya001@gmail.com>
2026-07-01 11:45:52 +0530

    feat: add user serialization and deserialization for passport authentication

--- src/config/passport.js
+++ src/config/passport.js
@@ -29,5 +29,18 @@ passport.use(
     }
       ),
 )
+passport.serializeUser((user, done) => {
+    done(null, user._id)
+})
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id)
+        done(null, user)
+
+    } catch (error) {
+        done(error, null)
+    }
+})
 
 export default passport
